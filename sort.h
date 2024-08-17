14be0939ffee90bddbd6b015a32689f6516895c5
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-16 23:43:43 -0500

    Add file and add program from notes

--- sort.h
+++ sort.h
@@ -49,5 +49,6 @@ void print_list(const listint_t *list)
 
 void bubble_sort(init *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
